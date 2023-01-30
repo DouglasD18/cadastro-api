2d2f5b902b06c2c3c87345267df0ca6e3a4ede85
Douglinhas <daguiaralcantara@gmail.com>
2023-01-30 19:42:43 -0300

    test: testa o body da resposta em caso de não envio do parâmetro email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,4 +19,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   })
-});
+
+  it('Should return 400 if no name is provided.', () => {
+    // given
+    const signUpController = new SignUpController();
+    const httpRequest = {
+      bode: {
+        name: "John Doe",
+        password: "my_password",
+        passwordConfirmation: "my_password"
+      }
+    }
+
+    // when
+    const httpResponse = signUpController.handle(httpRequest);
+
+    // then
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  })
+})
