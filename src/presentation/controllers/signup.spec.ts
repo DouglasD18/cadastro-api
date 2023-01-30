2f31b984ea4bc3aa644ca0ea20abb52ceed18fba
Douglinhas <daguiaralcantara@gmail.com>
2023-01-30 19:28:50 -0300

    test: criação do primeiro teste

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,21 @@
+import { SignUpController } from "./signup";
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if no name is provided.', () => {
+    // given
+    const signUpController = new SignUpController();
+    const httpRequest = {
+      bode: {
+        email: "doe@mail.com",
+        password: "my_password",
+        passwordConfirmation: "my_password"
+      }
+    }
+
+    // when
+    const httpResponse = signUpController.handle(httpRequest);
+
+    // then
+    expect(httpResponse.statusCode).toBe(400);
+  })
+});
