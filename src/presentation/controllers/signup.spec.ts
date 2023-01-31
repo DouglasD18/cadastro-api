246b317b3fb5a9bb0c5864ec6fa55b0470484740
Douglinhas <daguiaralcantara@gmail.com>
2023-01-30 21:15:47 -0300

    feat: criação dos erros para não envio de password ou passwordConfirmation e os devidos testes

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -21,7 +21,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'));
   })
 
-  it('Should return 400 if no name is provided.', () => {
+  it('Should return 400 if no email is provided.', () => {
     // given
     const signUpController = new SignUpController();
     const httpRequest = {
@@ -38,5 +38,43 @@ describe('SignUp Controller', () => {
     // then
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  })
+
+  it('Should return 400 if no password is provided.', () => {
+    // given
+    const signUpController = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "John Doe",
+        email: "doe@mail.com",
+        passwordConfirmation: "my_password"
+      }
+    }
+
+    // when
+    const httpResponse = signUpController.handle(httpRequest);
+
+    // then
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  })
+
+  it('Should return 400 if no passwordConfirmation is provided.', () => {
+    // given
+    const signUpController = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "John Doe",
+        email: "doe@mail.com",
+        password: "my_password",
+      }
+    }
+
+    // when
+    const httpResponse = signUpController.handle(httpRequest);
+
+    // then
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
   })
 })
