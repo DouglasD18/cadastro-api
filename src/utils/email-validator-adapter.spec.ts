b97bf1e835ea61e00376ed1e46a6bb5fb680a684
Douglinhas <daguiaralcantara@gmail.com>
2023-02-01 16:50:49 -0300

    git commit -m "test: add validator in EmailValidatorAdapter"

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { EmailValidatorAdapter } from "./email-validator";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+  isEmail(): boolean {
+    return true;
+  }
+}))
 
 describe("EmailValidator Adapter", () => {
   it("Should return false if validator return false", () => {
@@ -6,9 +13,21 @@ describe("EmailValidator Adapter", () => {
     const sut = new EmailValidatorAdapter();
 
     // when
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
 
     // then
     expect(isValid).toBe(false);
+  })
+
+  it("Should return true if validator return true", () => {
+    // given
+    const sut = new EmailValidatorAdapter();
+
+    // when
+    const isValid = sut.isValid("invalid_email@mail.com");
+
+    // then
+    expect(isValid).toBe(true);
   })
 });
