14d88c5a1f18f1fc66237dcb8b871475ebd2b090
Douglinhas <daguiaralcantara@gmail.com>
2023-02-02 14:31:44 -0300

    git commit -m "test: ensure BcryptAdapter returns a hash on success"

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,11 @@
-import bcrypt from 'bcrypt';
+import bcrypt, { hash } from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve("hashed_password"))
+  }
+}))
 
 describe("Bcrypt Adapter", () => {
   it("Should call bcrypt with correct value", async () => {
@@ -13,5 +19,17 @@ describe("Bcrypt Adapter", () => {
 
     // then
     expect(hashSpy).toHaveBeenCalledWith("valid_password", salt)
+  })
+
+  it("Should return a hash on success", async () => {
+    // given
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+
+    // when
+    const hash = await sut.encrypt("valid_password");
+
+    // then
+    expect(hash).toBe("hashed_password")
   })
 })
