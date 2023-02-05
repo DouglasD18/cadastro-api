1ad244458c0889a9a1386a84b950167889567d83
Douglinhas <daguiaralcantara@gmail.com>
2023-02-05 12:24:00 -0300

    feat: add ExpressRouteAdpter

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,8 +1,8 @@
-import { DbAddAccount } from "@/data/useCases/add-account/db-add-account";
-import { BcryptAdapter } from "@/infra/criptography/bcrypt-adapter";
-import { AccountMongoRepository } from "@/infra/db/mongodb/account-repository/account";
-import { SignUpController } from "@/presentation/controllers/signup/signup";
-import { EmailValidatorAdapter } from "@/utils/email-validator";
+import { DbAddAccount } from "../../data/useCases/add-account/db-add-account";
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { EmailValidatorAdapter } from "../../utils/email-validator";
 
 export const makeSignUpController = (): SignUpController => {
   const salt = 12;
