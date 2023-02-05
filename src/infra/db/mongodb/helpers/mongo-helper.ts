68cdcfced629e23eb826558f7881123a99a2cbc2
Douglinhas <daguiaralcantara@gmail.com>
2023-02-04 21:58:53 -0300

    feat: create AccountMongoRepository

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,17 @@
-import { MongoClient } from "mongodb"
+import { Collection, MongoClient } from "mongodb"
 
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(url)
   },
 
   async disconnect(): Promise<void> {
     await this.client.close();
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    return await this.client.db().collection(name);
   }
 }
